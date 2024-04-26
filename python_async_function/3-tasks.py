7375f3a62c3cfa41d52d9148104960f290f18566
AlxWrtl <indexes-benzine0p@icloud.com>
2024-04-26 17:47:31 +0200

    change the code by the exo 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python3
 """
-    Take the code from wait_n and alter it into a new function
-    task_wait_n.
-    The code is nearly identical to wait_n except task_wait_random
-    is being called.
+    Import wait_random from 0-basic_async_syntax.
+    Write a function (do not create an async function, use the regular function
+    syntax to do this) task_wait_random that takes an integer max_delay
+    and returns a asyncio.Task.
 """
 import asyncio
-from typing import List
 
-task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ Tasks wait_n"""
-    delays: List[float] = []
-    all_delays: List[float] = []
-    for i in range(n):
-        delays.append(task_wait_random(max_delay))
-
-    for delay in asyncio.as_completed(delays):
-        earliest_result = await delay
-        all_delays.append(earliest_result)
-
-    return all_delays
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """return a async.Task"""
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
